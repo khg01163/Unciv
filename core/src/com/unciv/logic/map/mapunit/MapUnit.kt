1d8c71646516d34495ce75e8e103424164619fb7
khg01163 <khg01163@gmail.com>
2025-10-02 20:13:12 +0900

    Update MapUnit.kt

--- core/src/com/unciv/logic/map/mapunit/MapUnit.kt
+++ core/src/com/unciv/logic/map/mapunit/MapUnit.kt
@@ -327,7 +327,14 @@ class MapUnit : IsPartOfGameInfoSerialization {
     ): Sequence<Unique> {
         return tempUniquesMap.getTriggeredUniques(trigger, gameContext, triggerFilter)
     }
-    
+
+
+
+    fun reload(
+        if (MapUnit.getTile==cityCenter.getTile) {
+            MapUnit.ammo == baseUnit.maxAmmo
+        }
+        )
 
     
     /** Gets *per turn* resource requirements - does not include immediate costs for stockpiled resources.
